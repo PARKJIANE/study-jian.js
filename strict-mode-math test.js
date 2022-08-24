818393fba04d81c3bc666689ebdb53e1829ed68d
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2022-08-24 21:37:40 +0900

    2022.08.24

---
+++ strict-mode-math test.js
@@ -0,0 +1,10 @@
+"use strict"
+function test() {
+    var num = "19"
+    console.log("NUMBER1: "+num) ; {
+        console.log("PLUS WHAT NUMBER")
+        let num = "39"
+        console.log("SUM: "+num)
+    }
+}
+test()
