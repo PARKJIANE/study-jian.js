ee3907b7ba01a45af3fcf24183f41fb5abf5a996
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2022-08-24 21:34:52 +0900

    2022.08.24

--- strict mode.js
+++ strict mode.js
@@ -1,7 +1,7 @@
 "use strict" 
 function test() { 
    var num = 100 
-   console.log("value of num in test() "+num); { 
+   console.log("value of num in test() "+num); {  //;안붙으면 SyntaxError 나옴
       console.log("Inner Block begins") 
       let num = 200 
       console.log("value of num : "+num)  
