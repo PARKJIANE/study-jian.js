64d5ed305638d4f52f82b1f34b142a9195e6b8e4
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2022-08-24 21:34:10 +0900

    Update strict mode.js

--- strict mode.js
+++ strict mode.js
@@ -1,7 +1,7 @@
 "use strict" 
 function test() { 
    var num = 100 
-   console.log("value of num in test() "+num) { 
+   console.log("value of num in test() "+num); { 
       console.log("Inner Block begins") 
       let num = 200 
       console.log("value of num : "+num)  
