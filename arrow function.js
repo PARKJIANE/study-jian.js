9090df3d469ebe489c12c9725f5efb66d2644a3a
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2022-08-30 13:01:59 +0900

    2022.08.30

---
+++ arrow function.js
@@ -0,0 +1,27 @@
+/* ES6 예제
+"use script"
+const A = (n1,n2) => n1+n2 
+console.log(A(10,15))
+
+const B = (n1) => {
+  if(n1%2==0)
+  return true
+  else
+  return false
+}
+console.log(B(8))
+*/
+
+/* 스스로 해보기
+"use script"
+const AA = (n1,n2) => n1/n2
+console.log(AA(20,5))
+
+const BB = (n1) => {
+  if(n1<=10)
+  return true
+  else
+  return false
+}
+console.log(BB(11))
+*/
